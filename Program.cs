e789baecb72d526755b3c4d2fe50605ba7ace143
Mubarak <12859241+mcmubby@users.noreply.github.com>
2021-05-18 12:58:03 +0100

    end of class

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace ExceptionHandling
 {
@@ -6,7 +7,22 @@ namespace ExceptionHandling
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Exception is unforeseen error when your program is executing
+            //Cons of not handling exception
+            //Your users get to see the error messages which is not intuitive
+            //The error messages contain valuable information that maybe useful to hackers
+            //Some lines of the code will be skipped.
+            try
+            {
+                StreamReader streamReader = new StreamReader("sample.txt");
+                Console.WriteLine(streamReader.ReadToEnd());
+                streamReader.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                // Console.WriteLine(ex.StackTrace);
+            }
         }
     }
 }
